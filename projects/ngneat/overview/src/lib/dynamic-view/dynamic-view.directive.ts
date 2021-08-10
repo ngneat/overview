dda0a96bc0c394f457e061f0f52399483ad89884
Netanel Basal <NetanelBasal@users.noreply.github.com>
2021-08-10 10:19:00 +0300

    fix: 🐛 use the directive injector if not provided

--- projects/ngneat/overview/src/lib/dynamic-view/dynamic-view.directive.ts
+++ projects/ngneat/overview/src/lib/dynamic-view/dynamic-view.directive.ts
@@ -48,7 +48,7 @@ export class DynamicViewDirective implements OnInit, OnChanges, OnDestroy {
     } else {
       this.viewRef = this.viewService.createView(this.view || this.defaultTpl, {
         vcr: this.vcr,
-        injector: this.injector,
+        injector: this.injector ?? this.vcr.injector,
         context: this.context,
       });
     }
