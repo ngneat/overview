05c164f8cb315e0d309f8e4a71d9dff0b85d9021
arturovt <arthurandrosovich@gmail.com>
2021-10-25 20:13:14 +0300

    feat: allow teleport bindings to be passed asynchronously

--- projects/ngneat/overview/src/lib/teleport/teleport-outlet.directive.ts
+++ projects/ngneat/overview/src/lib/teleport/teleport-outlet.directive.ts
@@ -1,20 +1,25 @@
-import { Directive, Input, OnDestroy, OnInit, ViewContainerRef } from '@angular/core';
+import { Directive, Input, OnChanges, OnDestroy, SimpleChanges, ViewContainerRef } from '@angular/core';
 import { TeleportService } from './teleport.service';
 
 @Directive({
   selector: '[teleportOutlet]',
 })
-export class TeleportOutletDirective implements OnInit, OnDestroy {
-  @Input() teleportOutlet: string;
+export class TeleportOutletDirective implements OnChanges, OnDestroy {
+  // We could've also used the `ngAcceptInputType`, but it's being deprecated in newer Angular versions.
+  @Input() teleportOutlet: string | null | undefined;
 
   constructor(private vcr: ViewContainerRef, private service: TeleportService) {}
 
-  ngOnInit() {
-    this.service.ports.set(this.teleportOutlet, this.vcr);
-    this.service.newOutlet(this.teleportOutlet);
+  ngOnChanges(changes: SimpleChanges): void {
+    // The `teleportOutlet` might be `null|undefined`, but we don't want nullable values to be used
+    // as keys for the `ports` map.
+    if (changes.teleportOutlet && typeof this.teleportOutlet === 'string') {
+      this.service.ports.set(this.teleportOutlet, this.vcr);
+      this.service.newOutlet(this.teleportOutlet);
+    }
   }
 
-  ngOnDestroy() {
+  ngOnDestroy(): void {
     this.service.ports.delete(this.teleportOutlet);
   }
 }
